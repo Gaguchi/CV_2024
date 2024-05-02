bbcc1546c3ad8cfdb578ea0bfdee4a1ba19e782d
Gaguchi <gaga9393@gmail.com>
2024-05-02 20:56:58 +0400

    update

--- App.jsx
+++ App.jsx
@@ -29,6 +29,8 @@ function App() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [modalContent, setModalContent] = useState(null);
   const [isAnimatingClose, setIsAnimatingClose] = useState(false);
+  const [SVGisActive, setSVGActive] = useState(false);
+
 
   useEffect(() => {
     if (isAnimatingClose) {
@@ -59,9 +61,9 @@ function App() {
     case 'harvard':
       content = (
         <>
-        <img class="rounded-t-lg max-h-xl w-full" src="/images/1c-1.png" alt="" />
-        <div class="p-6">
-          <h3 class="EU">CS50W Harvard Final Project</h3>
+        <img className="rounded-t-lg max-h-xl w-full" src="/images/1c-1.png" alt="" />
+        <div className="p-6">
+          <h3 className="EU">CS50W Harvard Final Project</h3>
           <p>For my final project in CS50W at Harvard, I developed a mobile app for movie recommendations and a gamified movie selection system. This involved creating games similar to the Wheel of Fortune or Plinketto.</p>
           <p>I used <strong>Django</strong> and <strong>React</strong> for the app development, and integrated the <strong>TMDB API</strong> for movie data. Additionally, I generated my own API to make the system work.</p>
         </div>
@@ -71,8 +73,8 @@ function App() {
     case 'milnort':
       content = (
         <>
-          <img class="rounded-t-lg max-h-xl w-full" src="/images/1c-1.png" alt="" />
-          <div class="p-6">
+          <img className="rounded-t-lg max-h-xl w-full" src="/images/1c-1.png" alt="" />
+          <div className="p-6">
             <p>At <strong>Milnort</strong>, my role centered around maintaining and enhancing a production cost calculator. I used <strong>1C</strong> for raw data generation, and <strong>Excel</strong> and <strong>Microsoft PowerBI</strong> for data optimization and visualization.</p>
             <p>I also developed a web scraping system using <strong>Python</strong> and <strong>JavaScript</strong>, with libraries like <strong>BeautifulSoup</strong>, <strong>Selenium</strong>, and <strong>Playwright</strong>. This system gathered and standardized pricing data across all regions of Russia for a specific product category.</p>
           </div>
@@ -80,11 +82,12 @@ function App() {
       );
       break;
     case 'petroholod':
+      setSVGActive(true)
       content = (
         <>
-        <img class="rounded-t-lg max-h-xl w-full" src="/images/1c-1.png" alt="" />
-        <div class="p-6">
-          <h3 class="EU">Experience at Petroholod</h3>
+        <img className="rounded-t-lg max-h-xl w-full" src="/images/1c-1.png" alt="" />
+        <div className="p-6">
+          <h3 className="EU">Experience at Petroholod</h3>
           <p>During my tenure at <strong>Petroholod</strong>, I was involved in creating several webpages for different products of the company. These pages were primarily developed using <strong>PHP</strong> and served various purposes such as introducing new products to the market.</p>
           <p>Some of these webpages were also used for advertising and conducting contests. For instance, one such web app was designed to accept information from customers and assign winners using a built-in random number generator.</p>
         </div>
@@ -94,9 +97,9 @@ function App() {
   case 'iceberry':
     content = (
       <>
-      <img class="rounded-t-lg max-h-xl w-full" src="/images/1c-1.png" alt="" />
-      <div class="p-6">
-        <h3 class="EU">Work at Iceberry</h3>
+      <img className="rounded-t-lg max-h-xl w-full" src="/images/1c-1.png" alt="" />
+      <div className="p-6">
+        <h3 className="EU">Work at Iceberry</h3>
         <p>At <strong>Iceberry</strong>, I implemented an in-house training and testing system for the company's merchandisers. This was done using <strong>Django</strong> and was aimed at educating new employees, testing them, and providing a simple system for their daily work.</p>
         <p>To facilitate maintenance, I created a CMS system with several admin layers. This system was also built using <strong>Django</strong>.</p>
       </div>
@@ -106,9 +109,9 @@ function App() {
 case '3d':
   content = (
     <>
-    <img class="rounded-t-lg max-h-xl w-full" src="/images/1c-1.png" alt="" />
-      <div class="p-6">
-        <h3 class="EU">3D Modeling Experience</h3>
+    <img className="rounded-t-lg max-h-xl w-full" src="/images/1c-1.png" alt="" />
+      <div className="p-6">
+        <h3 className="EU">3D Modeling Experience</h3>
         <p>I have experience in creating 3D models using software like <strong>Blender</strong> (which is my preferred tool, although I also have experience with Autodesk 3DS Max). My focus is on creating visually appealing yet lightweight models that won't be taxing for mobile users.</p>
       </div>
     </>
@@ -117,9 +120,9 @@ case '3d':
     case 'agro':
       content = (
         <>
-        <img class="rounded-t-lg max-h-xl w-full" src="/images/1c-1.png" alt="" />
-        <div class='p-6'>
-          <h3 class="EU">Agro E-commerce Project</h3>
+        <img className="rounded-t-lg max-h-xl w-full" src="/images/1c-1.png" alt="" />
+        <div className='p-6'>
+          <h3 className="EU">Agro E-commerce Project</h3>
           <p>For Agro, I developed a relatively simple e-commerce website using <strong>PHP</strong> and vanilla <strong>JavaScript</strong>. One of the key features of this site was an auto-updating price system.</p>
           <p>This system would dynamically change the price based on the quantity the user desired. It would inform the user how many more items they needed to purchase to qualify for a discount, and would display the discount percentage and the amount saved.</p>
         </div>
@@ -129,9 +132,9 @@ case '3d':
   case 'mksbonat':
     content = (
       <>
-      <img class="rounded-t-lg max-h-xl w-full" src="/images/1c-1.png" alt="" />
-      <div class='p-6'>
-        <h3 class="EU">Experience at MKSBonat</h3>
+      <img className="rounded-t-lg max-h-xl w-full" src="/images/1c-1.png" alt="" />
+      <div className='p-6'>
+        <h3 className="EU">Experience at MKSBonat</h3>
         <p>At <strong>MKSBonat</strong>, I maintained their e-commerce website, which was created with <strong>Drupal</strong>. My responsibilities included implementing updates to the site content and creating graphical designs for different products or events.</p>
         <p>One of my designs was used for a commercial truck, which was especially gratifying for me whenever I saw my design on the sides and back of trucks throughout the city.</p>
       </div>
@@ -141,9 +144,9 @@ case '3d':
 case 'mutabalis':
   content = (
     <>
-    <img class="rounded-t-lg max-h-xl w-full" src="/images/1c-1.png" alt="" />
-    <div class='p-6'>
-    <h3 class="EU">Work for Mutabalis</h3>
+    <img className="rounded-t-lg max-h-xl w-full" src="/images/1c-1.png" alt="" />
+    <div className='p-6'>
+    <h3 className="EU">Work for Mutabalis</h3>
     <p>For <strong>Mutabalis</strong>, a data analytics firm based in London, UK, I created a straightforward informative website. I find it relatively easy to work with foreign companies and clients, especially English-speaking ones, due to my education at Brunel University and my confidence in my English communication skills.</p>
     </div>
     </>
@@ -381,11 +384,11 @@ useEffect(() => {
 
 {(isModalOpen || isAnimatingClose) && (
     <div className={`modal fixed w-full h-full top-0 left-0 flex items-center justify-center`}>
-      <div class="modal-overlay absolute w-full h-full bg-zinc-900 opacity-50" onClick={handleClose}></div>
+      <div className="modal-overlay absolute w-full h-full bg-zinc-900 opacity-50" onClick={handleClose}></div>
         
       <div className={`modal-container w-11/12 md:max-w-md mx-auto rounded-xl shadow-lg z-50 overflow-y-auto  ${isAnimatingClose ? 'modal-closing' : 'modal-opening'}`} onClick={e => e.stopPropagation()} style={{transform: `${isModalOpen ? 'scale(1)' : 'scale(0.7)'}`, transition: 'transform 0.3s ease-out'}}>
         
-        <div class="bg-grad-element sm:row-span-2 w-full rounded-xl border border-white border-opacity-10  duration-200 hover:border-opacity-0 hover:no-underline hover:shadow-lg text-white hover:bg-white hover:bg-opacity-10 sm:flex-col sm:hover:shadow-2xl modal-content modal-content">
+        <div className="bg-grad-element sm:row-span-2 w-full rounded-xl border border-white border-opacity-10  duration-200 hover:border-opacity-0 hover:no-underline hover:shadow-lg text-white hover:bg-white hover:bg-opacity-10 sm:flex-col sm:hover:shadow-2xl modal-content modal-content">
             {modalContent}
         </div>
       </div>
@@ -523,8 +526,8 @@ Throughout my career, I've had the opportunity to wear many hats and tackle a va
     </h2>
       <div ref={gridRef} className="grid grid-cols-1 sm:grid-cols-3 sm:grid-rows-4 gap-4">
 
-      <div  id='petroholod'  onClick={modalClick}  className="bg-grad-element sm:row-span-2 flex w-full items-center rounded-xl border border-white border-opacity-10 px-4 py-6  duration-200 hover:border-opacity-0 hover:no-underline hover:shadow-lg text-white hover:bg-white hover:bg-opacity-10 sm:flex-col sm:hover:shadow-2xl">
-        <div className='pt-44 md:pt-20 flex flex-col items-center justify-center relative w-full h-full'>
+      <div  id='petroholod'  onMouseEnter={() => setSVGActive(true)} onMouseLeave={() => setSVGActive(false)} onClick={modalClick}  className="bg-grad-element sm:row-span-2 flex w-full items-center rounded-xl border border-white border-opacity-10 px-4 py-6  duration-200 hover:border-opacity-0 hover:no-underline hover:shadow-lg text-white hover:bg-white hover:bg-opacity-10 sm:flex-col sm:hover:shadow-2xl">
+        <div  className='pt-44 md:pt-20 flex flex-col items-center justify-center relative w-full h-full'>
           <img className="absolute z-20 pointer-events-none overflow w-20 h-20 object-cover" id='tabl' src="/images/tablet.png" alt="Description of Image 2"/>
           <video className="absolute z-19 pointer-events-none w-20 h-20 object-cover " id='tabl-vid' src="/videos/cv_example_2_sm.mp4" autoPlay loop muted />
           <img className="absolute z-30 pointer-events-none w-20 h-20 object-cover " id='mobl' src="/images/phone.png"alt="Description of Image 3"/>
@@ -532,7 +535,11 @@ Throughout my career, I've had the opportunity to wear many hats and tackle a va
 
           <h1 className="z-40 text-center mt-4">Petroholod</h1>
           <p className="z-40 text-center text-xs mt-2">I used my web-design and graphic design skills to market the products of the company.</p>
-          <div id='moving-background' className='relative w-52 h-20 overflow-hidden bg-white rounded-2xl'></div>
+          <div id='moving-background' className='relative w-52 h-28 overflow-hidden bg-white rounded-2xl'></div>
+          <div className="svgmask">
+              <svg  id="petrosvg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 106.45 102.64" width="106.44999694824219" height="102.63999938964844"><g id="seal"><path id="ball-line" d="M56.54,27.32c-7.23.1-13.12-6.15-13.21-13.21-.1-7.42,6.22-13.9,13.6-13.6,6.67.27,11.99,6,12.43,12.43.49,7.21-5.22,14.28-12.82,14.38Z" style={SVGisActive ?{ fill: "rgb(255, 255, 255, 0)", stroke: "rgb(255, 255, 255)", strokeMiterlimit: "10"}:{ fill: "rgb(255, 255, 255)", stroke: "rgb(255, 255, 255, 0)", strokeMiterlimit: "10"}} className={`${SVGisActive ? 'svg-elem-1-active':`svg-elem-1`}`}></path><path id="body-line" d="M.69,102.03c1.31-3.97,4.09-10.39,10.17-15.54,3.97-3.35,8.04-4.83,16.18-7.8,6.23-2.27,15.73-5.73,21.7-6.26,1.77-.16,3.53-.73,7.05-1.86.53-.17,1.57-.52,2.5-1.45.15-.15.94-.97,1.38-2.31.33-1.02.3-1.85.26-2.88-.05-1.17-.24-2.01-.47-3.01-.25-1.1-.65-2.48-1.31-4.13-.39-.97-.8-1.81-1.62-3.46-1.63-3.27-1.78-3.26-2.39-4.78,0,0-.74-1.86-1.21-3.85-.25-1.02-.37-1.54-.37-1.76,0-.11.01-.41-.07-.43-.08-.02-.2.22-.26.34-.3.57-.27.3-.91,1.34-.49.8-.79,1.29-1.08,2-.31.76-.31,1.06-.69,1.43-.16.15-.53.51-1.07.51-.07,0-.63,0-.97-.43-.22-.26-.27-.59-.26-.91,0-.28.07-1.61,1.66-3.84.88-1.23,1.32-1.85,2-2.26.13-.08.58-.34,1.03-.84.32-.36.75-.96.61-1.11-.12-.14-.68.15-1.02.32-.68.36-1.17.78-1.47,1.07-.51.49-.97,1.06-1.16,1.29-.45.56-.57.77-.91.91-.52.21-1.03.02-1.13-.02-.13-.05-.71-.28-.79-.75-.06-.33.15-.61.35-.88.73-.96,1.09-1.43,1.48-1.8,1.35-1.26,2.02-1.9,2.88-2.29.55-.25,1.1-.41,1.83-.95.48-.35.8-.68.89-.78.3-.32.39-.49,1.16-1.72,1.03-1.65.99-1.56,1.01-1.6.12-.28.22-.6.55-.85.09-.07.47-.33.83-.22.03.01.1.04.38.26.21.17.31.26.49.45.32.33.78.63,1.26.94,1.07.69,2.07,1.07,2.4,1.18.57.2,1.47.51,2.66.6.6.04.72,0,1.53.06.41.04,1.07.09,1.9.28.41.09,1.23.31,2.21.75.46.2,1.05.51,2.23,1.3,1.56,1.05,2.68,1.97,2.9,2.15.53.44,3.08,2.58,5.13,4.91,4.52,5.14,7.12,11.8,7.46,12.69,1.11,2.91,3.64,9.74,2.95,18.8-.28,3.72-.74,9.76-4.72,16,0,0-3.9,6.12-12.05,10.64-.31.17-.64.25-.64.25-.38.1-.69.07-.77.07-1.43-.08-17.59,0-18.28,0-.33,0-.66-.04-.98,0-.4.04-.63.12-.81-.02-.14-.11-.23-.32-.17-.46.06-.13.21-.15.46-.24.06-.02.26-.14.68-.37,1.18-.66,2.13-1.33,2.13-1.33.35-.24,1.05-.74,1.83-1.43.59-.52,1.84-1.65,2.98-3.44.78-1.24,1.18-2.26,1.57-3.27.62-1.62.91-2.91,1.11-3.8,0,0,.56-2.54.67-5.12.06-1.47-.12-1.57.02-2.03.02-.08.11-.33-.02-.49-.1-.12-.31-.16-.46-.09-.18.08-.24.29-.29.53-.15.69-.11,1.18-.14,1.7-.01.23-.06.51-.14,1.08,0,0-.14.92-.34,1.78-.14.63-.6,2.4-1.38,4.13-.29.63-1.58,3.37-4.35,6.12-2.4,2.37-4.65,3.5-7.12,4.75-2.66,1.34-4.47,1.82-5.98,2.04,0,0-.76.11-1.54.16-4.08.24-33.95.06-41.08.02Z" style={SVGisActive ?{ fill: "rgb(255, 255, 255, 0)", stroke: "rgb(255, 255, 255)", strokeMiterlimit: "10"}:{ fill: "rgb(255, 255, 255)", stroke: "rgb(255, 255, 255, 0)", strokeMiterlimit: "10"}} className={`${SVGisActive ? 'svg-elem-2-active':`svg-elem-2`}`}></path><path id="foot-line" d="M78.73,101.9c-.12-.21.51-.79.74-1,.37-.34.56-.51,1.01-.77,1.17-.66,3.02-2.79,3.55-3.4,2.63-3.02,4.2-6.56,4.63-7.58,1.66-3.92,2.24-7.61,2.28-8.27.02-.29.05-.45.12-.75.15-.68.24-1.11.53-1.21.36-.12.82.36.91.45.22.23.31.46.43.74.62,1.48.54,1.22.63,1.48.36,1.06.52,1.93.54,2.06.33,1.77,1,3.87,1.32,4.85.42,1.29,1.37,4.15,3.65,6.88,1.38,1.65,2.87,2.83,2.87,2.83,2.21,1.76,3.32,2.64,3.35,2.65.13.05.61.24.65.57.02.17-.07.37-.22.46-.18.11-.37.04-.66,0-.32-.05-.65,0-.98,0h-24.34c-.7.03-.95.12-1.02,0Z" style={SVGisActive ?{ fill: "rgb(255, 255, 255, 0)", stroke: "rgb(255, 255, 255)", strokeMiterlimit: "10"}:{ fill: "rgb(255, 255, 255)", stroke: "rgb(255, 255, 255, 0)", strokeMiterlimit: "10"}} className={`${SVGisActive ? 'svg-elem-3-active':`svg-elem-3`}`}></path><path id="foot" d="M93.59,86.78c-.62,3.41-.84,6.84,1.06,9.94,1.48,2.54,3.8,3.43,6.75,3.54q-5.8-5.31-7.81-13.48Z" style={{ fill: "rgb(31, 32, 33)", strokeWidth: "0px" }} className={`${SVGisActive ? 'active':``} svg-elem-4`}></path><path id="body" d="M86.08,58.82l-.32.11c0,2.87.1,5.75,0,8.63-.11,6.16-1.66,12.21-4.53,17.66-3.49,6.53-8.76,10.84-15,14.14-.55.3-1.08.63-1.59,1l.11.56c1.11.11,2.22.16,3.33.15,7.97-1.33,14.66-6.74,17.62-14.25,2.85-6.3,3.16-12.93,2.21-19.67-.54-2.89-1.27-5.67-1.83-8.33ZM29.97,82.47l-.1-.34c-.45-.09-.91-.13-1.37-.11-6,.56-11.72,2-16.68,5.86-2.75,2.1-5.17,4.64-6,8.28-.41,1.3-.14,2.71.73,3.76l.32-.66c2.46-3.74,5.7-6.9,9.49-9.28,3.8-2.32,7.81-4.31,11.72-6.53.65-.28,1.29-.62,1.89-1v.02ZM56.34,32.2c-1.46,1.54-2.63,4.81-2.94,8.06-.22,1.62.13,3.26,1,4.65.37.39.77.76,1.2,1.09l.31-.21c-1.83-4.65-.94-9.07.43-13.59h0ZM67.65,40.37c-.06-1.16-.96-2.1-2.12-2.2-1.11.06-1.99.97-2,2.09-.08,1.13.76,2.12,1.89,2.22h.11c1.16-.03,2.09-.95,2.12-2.11h0Z" style={{ fill: "rgb(31, 32, 33)", strokeWidth: "0px" }} className={`${SVGisActive ? 'active':``} svg-elem-5`}></path><path id="ball" d="M63.84,5.35c-2.4-3.75-10.13-4.52-13.92-1.77-4.54,3.32-6.44,11.49-3.49,15.65.53-4.65,1.2-9.07,4.81-12.16s8.09-2.49,12.6-1.72Z" style={{ fill: "rgb(31, 32, 33)", strokeWidth: "0px" }} className={`${SVGisActive ? 'active':``} svg-elem-6`}></path></g></svg>
+          </div>
+
         </div>
       </div>
 
