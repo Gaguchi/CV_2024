f251d56705549cb2f4424058758fecefb94b862e
Gaguchi <gaga9393@gmail.com>
2024-06-12 00:42:38 +0400

    modal and contacts

--- App.jsx
+++ App.jsx
@@ -605,7 +605,7 @@ useEffect(() => {
       <div className="modal-overlay absolute w-full h-full bg-zinc-900 opacity-50" onClick={handleClose}></div>
         
       <div className={`modal-container w-11/12 md:max-w-3xl mx-auto rounded-xl shadow-lg z-50 overflow-visible  ${isAnimatingClose ? 'modal-closing' : 'modal-opening'}`} onClick={e => e.stopPropagation()} style={{transform: `${isModalOpen ? 'scale(1)' : 'scale(0.7)'}`, transition: 'transform 0.3s ease-out'}}>
-        
+            <button className="closeButton" onClick={handleClose}>X</button>
             {modalContent}
       </div>
     </div>
@@ -926,7 +926,6 @@ useEffect(() => {
 
     <ContactForm />
 
-
       <div className="mt-8 w-full border-t border-gray-800 p-2 pt-8 text-center">
         <a className="text-indigo-400" href="mailto:Contact@BorisKaraia.com">Contact@BorisKaraia.com</a>
         <p className="my-5 leading-normal">Tbilisi<br />Georgia</p>
@@ -939,12 +938,6 @@ useEffect(() => {
           <a className="ml-4 text-gray-500">
             <svg fill="currentclass" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="h-5 w-5" viewBox="0 0 24 24">
               <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-            </svg>
-          </a>
-          <a className="ml-4 text-gray-500">
-            <svg fill="none" stroke="currentclass" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="h-5 w-5" viewBox="0 0 24 24">
-              <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
-              <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
             </svg>
           </a>
           <a className="ml-4 text-gray-500">
